ca3cd95d15b4786b81d02d470b3cfdd99600664f
rsbisne <roshansb99@gmail.com>
2023-07-07 15:45:40 +0530

    created pomodoro clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
 function App() {
+  const [seconds, setSeconds] = useState(25 * 60);
+  const [paused, setPaused] = useState(true);
+  const [isBreak, setIsBreak] = useState(false);
+
+  useEffect(() => {
+    const int = setInterval(() => {
+      if (!paused) {
+        setSeconds((s) => s - 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(int);
+    };
+  }, [paused]);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      setIsBreak((b) => !b);
+      setSeconds(isBreak ? 25 * 60 : 5 * 60);
+    }
+    // eslint-disable-next-line
+  }, [seconds]);
+
+  function startTimer() {
+    setPaused(false);
+  }
+
+  function pauseTimer() {
+    setPaused(true);
+  }
+
+  function resetTimer() {
+    setPaused(true);
+    setIsBreak(false);
+    setSeconds(25 * 60);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="timer">
+        <h1>{`${Math.floor(seconds / 60)}:${("00" + (seconds % 60)).slice(
+          -2
+        )}`}</h1>
+        <p>{isBreak ? "Break! Time." : "Work! Time."}</p>
+        <button onClick={paused ? startTimer : pauseTimer}>
+          {paused ? "Start" : "Pause"}
+        </button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </>
   );
 }
-
 export default App;
